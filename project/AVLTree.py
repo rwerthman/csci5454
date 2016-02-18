a621b0f28a69e9b5ba5ca6918cff0e750f61f57f
gddmk42171822 <Bobby@Roberts-MacBook-Pro.local>
2016-02-17 21:06:52 -0700

    started project with class definitions

---
+++ project/AVLTree.py
@@ -0,0 +1,17 @@
+class AVLTree(object):
+
+	def __init__(self):
+
+	def insert(self):
+
+	def find(self):
+
+	def delete(self):
+
+	def traverse(self):
+
+
+
+class AVLNode(object):
+
+	def __init__(self):
