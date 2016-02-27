45377f5f45cfde224fd431bd6bbb21265d0f23c2
gddmk42171822 <Bobby@Roberts-MacBook-Pro.local>
2016-02-26 22:32:37 -0700

    i think delete is correct

--- project/AVLTree.py
+++ project/AVLTree.py
@@ -1,159 +1,209 @@
 class AVLTree(object):
 
-	def __init__(self):
-		self.root = None
+  def __init__(self):
+    self.root = None
 
-	def Insert(self, key):
-		'''
-		Insert a key into an AVL tree
+  def Insert(self, key):
+    '''
+    Insert a key into an AVL tree
 
-		Args:
-		  self
-		  key 
+    Args:
+    self
+    key 
 
-		Returns:
+    Returns:
 
-		Sources:
-		  Introduction to Algorithms, Third Edition (page 294)
-		    - Non-recursive insert of a node into a binary tree
+    Sources:
+    Introduction to Algorithms, Third Edition (page 294)
+    - Non-recursive insert of a node into a binary tree
 
-		'''
+    '''
 
-		# Create a new node with the key argument of insert
-		new_node = AVLNode(key) 
+    # Create a new node with the key argument of insert
+    new_node = AVLNode(key) 
 
-		parent_node = None # This will be used to store the parent node
-		current_node = self.root # Start the insert at root of the tree
+    parent_node = None # This will be used to store the parent node
+    current_node = self.root # Start the insert at root of the tree
 
-		# If there are nodes in the tree
-		while current_node is not None:
+    # If there are nodes in the tree
+    while current_node is not None:
 
-			parent_node = current_node # Set y to the parent node
+      parent_node = current_node # Set y to the parent node
 
-			# Insert into the left part of the tree if the key of the new node 
-			# is less than the key of the current node we are looking at
-			if new_node.key < current_node.key: 
-				current_node = current_node.left_child
-			else:
-				# Insert into the right part of the tree
-				# if the key of the new node is less than
-				# the key of the current node we are looking at
-				current_node = current_node.right_child
+      # Insert into the left part of the tree if the key of the new node 
+      # is less than the key of the current node we are looking at
+      if new_node.key < current_node.key: 
+        current_node = current_node.left_child
+      else:
+      # Insert into the right part of the tree
+      # if the key of the new node is less than
+      # the key of the current node we are looking at
+        current_node = current_node.right_child
 
-		new_node.parent = parent_node # Set the parent node of the new node
+    new_node.parent = parent_node # Set the parent node of the new node
 
-		# Set the root node of the tree to the new node
-		# if the tree is empty
-		if parent_node is None:
-			self.root = new_node
-		# Set the left child of the parent node to the new node
-		elif new_node.key < parent_node.key:
-			# Set the height of the new node
-			new_node.height = parent_node.height + 1 
-			parent_node.left_child = new_node
-		# Set the right child of the parent to the new node
-		else:
-			# Set the height of the new node
-			new_node.height = parent_node.height + 1 
-			parent_node.right_child = new_node
+    # Set the root node of the tree to the new node
+    # if the tree is empty
+    if parent_node is None:
+      self.root = new_node
+    # Set the left child of the parent node to the new node
+    elif new_node.key < parent_node.key:
+    # Set the height of the new node
+      new_node.height = parent_node.height + 1 
+      parent_node.left_child = new_node
+    # Set the right child of the parent to the new node
+    else:
+    # Set the height of the new node
+      new_node.height = parent_node.height + 1 
+      parent_node.right_child = new_node
 
-		# TODO: Need to balance the tree after an insert
+    # TODO: Need to balance the tree after an insert
 
 
 
-	def Find(self, key):
-		'''
+  def Find(self, key):
+	
+    # Check if the tree is empty
+    if self.root is None:
+      print 'Find: Tree is empty.'
+      return
 
-		'''		
-		# Check if the tree is empty
-		if self.root is None:
-			print 'Find: Tree is empty.'
-			return
+    current_node = self.root # Start looking for the key at the root node
 
-		current_node = self.root # Start looking for the key at the root node
-
-		while current_node is not None:
-			if key == current_node.key:
-				return current_node
-			if key < current_node.key:
-				current_node = current_node.left_child
-			else:
-				current_node = current_node.right_child
-
-		return None
+    while current_node is not None:
+      if key == current_node.key:
+        return current_node
+      elif key < current_node.key:
+        current_node = current_node.left_child
+      else:
+        current_node = current_node.right_child
+    return None
 
 
 
 
 
-	def Delete(self, key):
-		'''
+  def Delete(self, key):
 
+    # Check if the tree is empty
+    if self.root is None:
+      print 'Delete: Tree is empty.'
+      return
 
-		'''
+    # Node to be removed
+    r_node = self.Find(key)
 
-		# Check if the tree is empty
-		if self.root is None:
-			print 'Delete: Tree is empty.'
-			return
+    if r_node is None:
+    	print 'Delete: Key not found in tree.'
+    	return
 
-		current_node = self.Find(key)
+    # If the key is found in a leaf node
+    if r_node.right_child is None and r_node.left_child is None:
+    	# Check the which child the leaf node belongs to and delete it
+    	self.Transplant(r_node, None)
+      
+    # If the key is found in a node with only a left child
+    elif r_node.right_child is None and r_node.left_child is not None:
+      self.Transplant(r_node, r_node.left_child)
 
-		# If the key is found in a leaf node
-		if current_node.right_child is None and current_node.left_child is None:
-			# Check the which child the leaf node belongs to and delete it
-			if current_node.parent.right_child is current_node:
-				current_node.parent.right_child = None
-			else:
-				current_node.parent.left_child = None
+    # If the key is found in a node with only a right child
+    elif r_node.right_child is not None and r_node.left_child is None:
+      self.Transplant(r_node, r_node.right_child)
 
+    # If the key is found in a node with both a left and a right child
+    else:
+      successor = self.FindSuccessor(r_node)
 
-		
+      # Check if the successor is not the right child of the node we are deleting
+      if successor.parent is not r_node:
+      	self.Transplant(successor, successor.right_child)
+      	successor.right_child = r_node.right_child
+      	successor.right_child.parent = successor
 
-	def PrintTree(self, current_node):
-		'''
-
-		Description:
-
-		Args:
-
-		Returns:
-
-		Sources: 
-		  https://stackoverflow.com/questions/20242479/printing-a-tree-data-
-		  												structure-in-python
-		    - How to print a tree data structure that is understandable
-		'''
-
-		# Check if the tree is empty
-		if self.root is None:
-			print 'PrintTree: Tree is empty.'
-			return
-
-		if current_node is not None:
-
-			# Print the right side of the parent
-			if current_node.right_child is not None:
-				self.PrintTree(current_node.right_child)
-				print '\t' * current_node.height + '    /'
-
-			# Print the parent node
-			print '\t' * current_node.height + str(current_node.key)
-
-			# Print the left side of the parent
-			if current_node.left_child is not None:
-				print '\t' * current_node.height + '    \\'
-				self.PrintTree(current_node.left_child)
+      self.Transplant(r_node,successor)
+      successor.left_child = r_node.left_child
+      successor.left_child.parent = successor
 
 
 
+  def FindSuccessor(self, node):
+  	if node.right_child is not None:
+  		# Set node to its right child
+  		node = node.right_child
+  		while node.left_child is not None:
+  			node = node.left_child
+  		return node
+
+
+  def Transplant(self, node, node_child):
+    # Check which child of the parent the current node belongs to
+
+    # If the node we are looking at is a leaf node no transplant necessary
+    if node_child is None:
+      if node.parent.right_child is node:
+        node.parent.right_child = None
+      else:
+        node.parent.left_child = None
+    # If the node is the parent's right child
+    elif node.parent.right_child is node:
+    	# Set the parent's right child to the child of the node
+      node.parent.right_child = node_child
+      # Set the child's of the node to it the node's parent
+      node_child.parent = node.parent
+    # If the node is the parent's left child
+    else:
+    	# Set the parent's right child to the child of the node
+      node.parent.left_child = node_child
+      # Set the child's of the node to it the node's parent
+      node_child.parent = node.parent
+
+  def PrintTree(self, current_node):
+    '''
+
+    Description:
+
+    Args:
+
+    Returns:
+
+    Sources: 
+    https://stackoverflow.com/questions/20242479/printing-a-tree-data-
+										structure-in-python
+    - How to print a tree data structure that is understandable
+    '''
+
+    # Check if the tree is empty
+    if self.root is None:
+      print 'PrintTree: Tree is empty.'
+      return
+
+    if current_node is not None:
+
+      # Print the right side of the parent
+      if current_node.right_child is not None:
+        self.PrintTree(current_node.right_child)
+        print '\t' * current_node.height + '    /'
+
+      # Print the parent node
+      print '\t' * current_node.height + str(current_node.key)
+
+      # Print the left side of the parent
+      if current_node.left_child is not None:
+        print '\t' * current_node.height + '    \\'
+        self.PrintTree(current_node.left_child)
+
+  def UpdateHeights(self):
+  	pass
+
+  def Balance(self):
+  	pass
 
 
 class AVLNode(object):
 
-	def __init__(self, key):
-		self.left_child = None
-		self.right_child = None
-		self.parent = None
-		self.key = key
-		self.height = 0
+  def __init__(self, key):
+	self.left_child = None
+	self.right_child = None
+	self.parent = None
+	self.key = key
+	self.height = 0
--- project/TestAVLTree.py
+++ project/TestAVLTree.py
@@ -1,72 +1,148 @@
 from AVLTree import AVLTree
 
 def CreateTree():
-	tree = AVLTree()
+  tree = AVLTree()
 
-	tree.Insert(5) # Root node
+  tree.Insert(5) # Root node
 
-	tree.Insert(3)
-	tree.Insert(4)
-	tree.Insert(6)
-	tree.Insert(5)
-	tree.Insert(7)
-	tree.Insert(8)
-	tree.Insert(2)
-	tree.Insert(1)
+  tree.Insert(3)
+  tree.Insert(4)
+  tree.Insert(6)
+  tree.Insert(5.5)
+  tree.Insert(7)
+  tree.Insert(6.5)
+  tree.Insert(8)
+  tree.Insert(2)
+  tree.Insert(1)
 
-	return tree
+  return tree
 
 
 def TestFind():
-	'''
-	'''
+  
+  tree = CreateTree()
+
+  node = tree.Find(10)
+  Test('Test Find method with key not in tree.', node is None)
+
+  node = tree.Find(2)
+  Test('Test Find method with key in tree.', node.key == 2)
+
+def TestFindSuccessor():
+	print bcolors.BOLD + bcolors.UNDERLINE + bcolors.OKBLUE + 'Tests for FindSuccessor' + bcolors.ENDC
 	tree = CreateTree()
+	tree.PrintTree(tree.root)
 
-	node = tree.Find(10)
-	Test('Test Find method with key not in tree.', node is None)
+	node = tree.Find(6)
+	successor = tree.FindSuccessor(node)
+	node = tree.Find(6.5)
+	Test('Test FindSuccessor: Node is correct successor.', successor is node)
 
-	node = tree.Find(2)
-	Test('Test Find method with key in tree.', node.key == 2)
+	node = tree.Find(5)
+	successor = tree.FindSuccessor(node)
+	node = tree.Find(5.5)
+	Test('Test FindSuccessor: Node is correct successor.', successor is node)
+
 
 def TestDelete():
-	'''
 
-	'''
+  #
+  # Tests for deleting a leaf node
+  #
+  print bcolors.BOLD + bcolors.UNDERLINE + bcolors.OKBLUE + 'Tests for deleting a leaf node' + bcolors.ENDC
 
-	#
-	# Tests for deleting a leaf node
-	#
-	tree = CreateTree()
-	tree.PrintTree(tree.root)
+  tree = CreateTree()
+  tree.PrintTree(tree.root)
 
-	tree.Delete(8)
-	tree.PrintTree(tree.root)
-	node = tree.Find(8)
-	Test('Test Delete method with leaf node.', node is None)
+  tree.Delete(8)
+  tree.PrintTree(tree.root)
+  node = tree.Find(8)
+  Test('Test Delete method with leaf node.', node is None)
 
-	tree.Delete(4)
-	tree.PrintTree(tree.root)
-	node = tree.Find(4)
-	Test('Test Delete method with leaf node.', node is None)
+  tree.Delete(4)
+  tree.PrintTree(tree.root)
+  node = tree.Find(4)
+  Test('Test Delete method with leaf node.', node is None)
 
-	tree.Delete(1)
-	tree.PrintTree(tree.root)
-	node = tree.Find(1)
-	Test('Test Delete method with leaf node.', node is None)
+  tree.Delete(1)
+  tree.PrintTree(tree.root)
+  node = tree.Find(1)
+  Test('Test Delete method with leaf node.', node is None)
+
+  #
+  # Tests for deleting node with single child
+  #
+  print bcolors.BOLD + bcolors.UNDERLINE + bcolors.OKBLUE + 'Tests for deleting node with single child' + bcolors.ENDC
+
+  tree = CreateTree()
+  tree.Delete(2)
+  tree.PrintTree(tree.root)
+  node = tree.Find(2)
+  Test('Test Delete method with left child: Node is removed.', node is None)
+  parent = tree.Find(3)
+  child = tree.Find(1)
+  Test('Test Delete method with left child: Child is correct.', parent.left_child is child)
+  Test('Test Delete method with left child: Parent is correct.', child.parent is parent)
+
+  tree.Delete(7)
+  tree.PrintTree(tree.root)
+  node = tree.Find(7)
+  Test('Test Delete method with right child: Node is removed.', node is None)
+  parent = tree.Find(6)
+  child = tree.Find(8)
+  Test('Test Delete method with right child: Child is correct.', parent.right_child is child)
+  Test('Test Delete method with right child: Parent is correct.', child.parent is parent)
 
 
+  #
+  # Tests for deleting node with both children
+  #
+  print bcolors.BOLD + bcolors.UNDERLINE + bcolors.OKBLUE + 'Tests for deleting node with two children' + bcolors.ENDC
+
+  tree = CreateTree()
+  tree.Delete(6)
+  tree.PrintTree(tree.root)
+
+  node = tree.Find(6)
+  Test('Test Delete method with both children: Node is removed.', node is None)
+  parent = tree.Find(5)
+  child = tree.Find(6.5)
+  Test('Test Delete method with both children: Child is correct.', parent.right_child is child)
+  Test('Test Delete method with both children: Parent is correct.', child.parent is parent)
+
+  parent = tree.Find(6.5)
+  child = tree.Find(5.5)
+  Test('Test Delete method with both children: Child is correct.', parent.left_child is child)
+  Test('Test Delete method with both children: Parent is correct.', child.parent is parent)
+
+  parent = tree.Find(6.5)
+  child = tree.Find(7)
+  Test('Test Delete method with both children: Child is correct.', parent.right_child is child)
+  Test('Test Delete method with both children: Parent is correct.', child.parent is parent)
+
+
+class bcolors:
+  HEADER = '\033[95m' # Purple
+  OKBLUE = '\033[94m' # Blue
+  OKGREEN = '\033[92m' # Green
+  WARNING = '\033[93m' # Yellow
+  FAIL = '\033[91m' # Red
+  ENDC = '\033[0m' # Black
+  BOLD = '\033[1m' # Bold
+  UNDERLINE = '\033[4m' # Underline
 
 def Test(testName, bool):
-	if bool:
-		print testName + " Passed."
-	else:
-		print testName + " Failed."
+  if bool:
+    print bcolors.OKGREEN + testName + " Passed." + bcolors.ENDC
+  else:
+    print bcolors.FAIL + testName + " Failed." + bcolors.ENDC
 
 
 def main():
-	TestFind()
-	TestDelete()
+  TestFind()
+  TestDelete()
+  TestFindSuccessor()
 
 
 if __name__ == '__main__':
-	main()
+  main()
