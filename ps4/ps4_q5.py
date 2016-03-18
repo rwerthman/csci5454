124a638f1b873e13db79a2342f3733406729f1c1
gddmkr42171822 <rwghost98@gmail.com>
2016-03-17 22:04:33 -0600

    Calling it good.

--- ps4/ps4_q5.py
+++ ps4/ps4_q5.py
@@ -1,3 +1,9 @@
+'''
+Robert Werthman
+CSCI 5454
+PS4 Question 5
+'''
+
 import numpy as np
 import math
 
@@ -25,24 +31,36 @@ def ChooseAction(weights):
 
 def Hedge():
   # Initialize pay off matrix
-  M = np.array([[ 0, 1,-1],
-                [-1, 0, 1],
-                [ 1,-1, 0]])
+#   M = np.array([[ 0, 1,-1],
+#                 [-1, 0, 1],
+#                 [ 1,-1, 0]])
+  
+  M = np.array([[1,0,1,8,0],
+                [5,8,9,2,1],
+                [0,1,8,0,5],
+                [8,9,2,1,0],
+                [1,8,0,5,8]])
+#   M = np.array([[1,2],
+#                 [1,2]])
     
   # The number of times hedge will run to find the nash equilibrium
-  rounds = 3
+  T = 25
   
   weights_size = M.shape[0]
   row_weights = np.ones((weights_size,1))
   column_weights = np.ones((weights_size,1))
+  learning_rate = 1.0/math.sqrt(T)
   
-  learning_rate = 1.0/math.sqrt(rounds)
+  row_payoff = np.zeros((weights_size,1))
+  column_payoff = np.zeros(weights_size)
   
   print 'initial row weights', row_weights
   print 'initial column weights', column_weights
   print 'initial learning rate', learning_rate
+  print 'initial row payoff', row_payoff
+  print 'initial column payoff', column_payoff
   
-  for t in range(rounds):
+  for t in range(T):
     print '##########'
     print 'Round', t
     print '##########'
@@ -56,21 +74,34 @@ def Hedge():
     row_loss = M.dot(column_distribution)
     column_loss = (row_distribution.T).dot(M)
     
-    row_nash_equalibrium = (row_distribution.T).dot(row_loss)
-    column_nash_equalibrium = column_loss.dot(column_distribution)
+    # Iteratively calculate the vector of expected payoffs
+    # for each player.  Every entry should sum to 0.
+    row_payoff = np.add(row_payoff, (row_distribution.T).dot(row_loss))
+    column_payoff = np.subtract(column_payoff, column_loss.dot(column_distribution))
     
-    print 'nash equalibrium found by row', row_nash_equalibrium
-    print 'nash equalibrium found by column', column_nash_equalibrium
-    
+    print 'row payoff', row_payoff
+    print 'column payoff', column_payoff
     
     print 'row loss', row_loss
     print 'column loss', column_loss
     
+    row_nash_equalibrium = (row_distribution.T).dot(row_loss)
+    column_nash_equalibrium = column_loss.dot(column_distribution)
+    
+#     print 'nash equalibrium found by row', row_nash_equalibrium
+#     print 'nash equalibrium found by column', column_nash_equalibrium
+    
+    
+    
+    
     row_weights = UpdateWeights(learning_rate, row_weights, row_loss)
     column_weights = UpdateWeights(learning_rate, column_weights, column_loss.T)
         
-    print 'update row weights', row_weights
-    print 'update column weights', column_weights
+#     print 'updated row weights', row_weights
+#     print 'updated column weights', column_weights
+    
+  print 'time-averaged expected payoff row', row_nash_equalibrium/T
+  print 'time-averaged expected payoff column', column_nash_equalibrium/T
     
 
 def main():
